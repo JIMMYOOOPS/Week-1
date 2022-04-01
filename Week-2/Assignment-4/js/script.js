b2a0a2ccd64687829f70eb39c834c01765bb1b4b
jimmyoops <jimmy85030@gmail.com>
2022-04-01 15:21:44 +0800

    update assignment-4 for week-2

---
+++ Week-2/Assignment-4/js/script.js
@@ -0,0 +1,9 @@
+let bannerMain = document.querySelector("h1")
+
+let bannerWordChange = () => {bannerMain.innerText = "Have a Good Day in Pokemon World";}
+bannerMain.addEventListener("click", bannerWordChange);
+
+let contentbtn = document.querySelector("button")
+let showcontentbtn = () => {document.querySelector(".contentboxbtm").style.display  = "flex";}
+contentbtn.addEventListener("click", showcontentbtn)
+
